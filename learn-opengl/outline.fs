30c5c57b47a6739eec0742afa6c24b0c47cb1257
eathom91 <eathom91@gmail.com>
2024-01-15 19:36:18 -0500

    added stencil based outlining

--- learn-opengl/outline.fs
+++ learn-opengl/outline.fs
@@ -7,6 +7,5 @@ uniform sampler2D texture1;
 
 void main()
 {    
-    FragColor = texture(texture1, TexCoords);
-    //FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+    FragColor = vec4(0.04, 0.28, 0.26, 1.0);
 }
